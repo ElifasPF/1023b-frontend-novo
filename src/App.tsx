df960bb61d5bd6eaecea4b579af55acbfd10c93f
ElifasPF <elifas.faustino@estudante.ifms.edu.br>
2025-09-17 11:51:57 -0400

    cadastro no front end

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,34 @@ function App() {
       .then((response) => response.json())
       .then((data) => setProdutos(data))
   }, [])
+  function handleForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+
+    fetch('/api/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then((response) => response.json())
+      .then((newProduto) => setProdutos([...produtos, newProduto]))
+      .catch((error) => console.error('Error:', error))
+    form.reset()
+  }
   return (
     <>
-    <div>Loja de Celulares</div>
+      <div>Cadastro de Produtos</div>
+      <form onSubmit={handleForm}>
+        <input type="text" name="nome" placeholder="Nome" />
+        <input type="number" name="preco" placeholder="Preço" />
+        <input type="text" name="urlfoto" placeholder="URL da Foto" />
+        <input type="text" name="descricao" placeholder="Descrição" />
+        <button type="submit">Cadastrar</button>
+      </form>
+      <div>Loja de Celulares</div>
     {
       produtos.map((produto) => (
         <div key={produto._id}>
