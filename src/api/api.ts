c216b942913a66bdabb62f5290205acb2e96bb18
ElifasPF <elifas.faustino@estudante.ifms.edu.br>
2025-10-20 11:52:40 -0400

    atualização

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,26 +1,28 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL
 })
 //Nós vamos criar um middleware para adicionar o token na requisição
 
-api.interceptors.request.use((config) =>{
+api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token")
-    if(token)
+    if (token)
         config.headers.Authorization = `Bearer ${token}`
     return config
 })
 
 //Redirecionar para o LOGIN quando o usuário não tiver permissão.
 api.interceptors.response.use(
-    (response)=>response,
-    (error)=>{
+    (response) => response,
+    (error) => {
+        if(error?.code === 'ERR_NETWORK'){
+            window.location.href = `/error?mensagem=${encodeURIComponent('Ligue o Servidor!')}`
+        }
         const status = error?.response?.status;
-        if(status===401&&!(error?.config?.url.endsWith('/login')
-        )){
+        if (status === 401 && !(error?.config?.url.endsWith('/login'))) {
             localStorage.removeItem("token")
-            window.location.href="/login?message=Token inválido!"       
+            window.location.href = `/login?mensagem=${encodeURIComponent('Token inválido!')}`
         }
         return Promise.reject(error)
     }
